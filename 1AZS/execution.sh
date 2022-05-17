465c6b853bc720b98f4520d9bb36d47e242c686b
Brian Jimenez <br.jimenezgarcia@gmail.com>
2022-05-17 15:01:26 +0000

    1AZS

---
+++ 1AZS/execution.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+COMPLEX=$1
+
+# Setup
+lightdock3_setup.py ${COMPLEX}_l_u.pdb ${COMPLEX}_r_u.pdb --noxt --noh -anm -anm_rec_rmsd 0.5 -anm_lig_rmsd 0.5
+
+# Convert ANM files
+python3 -c "import numpy as np; np.save('rec_nm.npy', np.load('lightdock_rec.nm.npy').flatten())"
+python3 -c "import numpy as np; np.save('lig_nm.npy', np.load('lightdock_lig.nm.npy').flatten())"
+
+# Prepare simulation
+s=`ls -d swarm_* | wc -l`
+swarms=$((s-1))
+for i in `seq 0 $swarms`;do echo "cd swarm_${i}; cp ../lightdock_${COMPLEX}_r_u.pdb .; cp ../lightdock_${COMPLEX}_l_u.pdb .;cp ../rec_nm.npy .;cp ../lig_nm.npy .; lightdock-rust ../setup.json ../init/initial_positions_${i}.dat 100 dfire; rm -rf lightdock_*.pdb *.npy;" >> task.list; done
+
